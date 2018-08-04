cf562eca00b2c205d1c4f51419bf7ac05410eafd
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-08-04 13:56:23 +0000

    Module 4 test complete

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -14,11 +15,21 @@ class Quiz extends Component {
     
     render(){
 
-        return (
-            <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-            </div>
-        )
+        const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length);
+        if (isQuizEnd) {
+            return (
+                <div>
+                    <QuizEnd />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+                </div>
+            )
+        }
+
     }
 }
 
